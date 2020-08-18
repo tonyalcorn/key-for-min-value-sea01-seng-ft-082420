37e5bbafa9cf86e94ea1c9dfef731ddbab6339ed
Anthony Alcorn <xmastony@gmail.com>
2020-08-18 05:07:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,14 +4,14 @@
 ikea = {:chair => 25, :table => 85, :mattress => 450}
 
 def key_for_min_value(name_hash)
-  lowest_value = 0
+  lowest_value = 500
   lowest_key = nil
   
   name_hash.each do |key,value|
     #if the lowest_value is in it's intial state  
     #or if the value of the current key is less than the lowest_value
     #then: set the lowest_value to value and lowest_key to key
-    if lowest_value == 0 || value < lowest_value
+    if lowest_value == 500 || value < lowest_value
       lowest_value = value 
       lowest_key = key
     end
